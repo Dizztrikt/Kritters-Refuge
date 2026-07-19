34513a48692bb85c7529ea4e4653eeca4d5aa79d
LinaPlusPlus <TenmaTengo@protonmail.com>
2026-07-19 14:56:10 -0400

    fix NPC related commands

--- Content.Server/NPC/Commands/NPCCommand.cs
+++ Content.Server/NPC/Commands/NPCCommand.cs
@@ -1,26 +1,57 @@
+﻿using Robust.Shared.Toolshed;
+using Content.Server.NPC.HTN;
+
 using Content.Server.Administration;
-using Content.Server.EUI;
-using Content.Server.NPC.UI;
 using Content.Shared.Administration;
-using Robust.Shared.Console;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Prototypes;
+
+using Content.Shared.CombatMode;
+using Robust.Shared.Toolshed.Syntax;
 
 namespace Content.Server.NPC.Commands;
 
-[AdminCommand(AdminFlags.Debug)]
-public sealed class NPCCommand : IConsoleCommand
+[ToolshedCommand, AdminCommand(AdminFlags.Fun)]
+public sealed class NPCCommand : ToolshedCommand
 {
-    public string Command => "npc";
-    public string Description => "Opens the debug window for NPCs";
-    public string Help => $"{Command}";
-    public void Execute(IConsoleShell shell, string argStr, string[] args)
-    {
-        if (shell.Player is not { } playerSession)
-        {
-            shell.WriteError(Loc.GetString("shell-cannot-run-command-from-server"));
-            return;
-        }
 
-        var euiManager = IoCManager.Resolve<EuiManager>();
-        euiManager.OpenEui(new NPCEui(), playerSession);
+    [Dependency] private readonly IEntityManager _entities = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private IEntitySystemManager _sysManager = default!;
+
+    [CommandImplementation("setBehavor")]
+    public void SetBehavor(
+        [PipedArgument] EntityUid entity,
+        [CommandArgument] ProtoId<HTNCompoundPrototype> behavor
+    )
+    {
+        var htnComponent = _entities.EnsureComponent<HTNComponent>(entity);
+
+
+        htnComponent.RootTask = new HTNCompoundTask()
+        {
+            Task = behavor.ToString()
+        };
+    }
+    [CommandImplementation("setCombatMode")]
+    public void SetCombatMode(
+        [PipedArgument] EntityUid entity,
+        [CommandArgument] bool enabled
+    )
+    {
+        var combatComponent = _entities.EnsureComponent<CombatModeComponent>(entity);
+        var combatSystem = _sysManager.GetEntitySystem<SharedCombatModeSystem>();
+        combatSystem.SetInCombatMode(entity, enabled, combatComponent);
+    }
+
+    [CommandImplementation("inCombatMode")]
+    public bool InCombatMode(
+        [PipedArgument] EntityUid entity
+    )
+    {
+        if (_entities.TryGetComponent<CombatModeComponent>(entity, out var combatComponent)) {
+            return combatComponent.IsInCombatMode;
+        }
+        return false;
     }
 }
